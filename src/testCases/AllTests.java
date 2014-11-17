e3787a36c00c8addf15dc97e0e04fbb3ea0dd698
ironhulk <ironiba@gmail.com>
2014-11-17 11:06:28 +0200

    Added test suite

--- src/testCases/AllTests.java
+++ src/testCases/AllTests.java
@@ -1,8 +1,13 @@
-package testCases.crudTests;
+package testCases;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import testCases.crudTests.CategoryCRUDTest;
+import testCases.crudTests.ProductsCRUDTest;
+import testCases.crudTests.StoreCRUDTest;
+import testCases.crudTests.StoreOwnerCRUDTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CategoryCRUDTest.class, ProductsCRUDTest.class,
